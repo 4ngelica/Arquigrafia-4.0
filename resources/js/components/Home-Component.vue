67156375d87bcf1b82f4f5291fde80eadbd8b322
Angélica Batassim <angelicabatassim@gmail.com>
2022-09-15 07:37:57 -0300

    feat: create makefile

--- resources/js/components/Home-Component.vue
+++ resources/js/components/Home-Component.vue
@@ -1,21 +1,58 @@
 <template>
+
   <div class="hero is-fullheight is-bold is-info">
+    <!-- <div class="container d-flex flex-row">
+      <div class="col-2 d-flex flex-column">
+        <label for="date">Data de inclusão</label>
+        <select class="" name="date">
+          <option value="">data</option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </div>
+      <div class="col-2 d-flex flex-column">
+        <label for="date">Data de inclusão</label>
+        <select class="" name="date">
+          <option value="">data</option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </div>
+      <div class="col-2 d-flex flex-column">
+        <label for="date">Data de inclusão</label>
+        <select class="" name="date">
+          <option value="">data</option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </div>
+      <div class="col-2 d-flex flex-column">
+        <label for="date">Data de inclusão</label>
+        <select class="" name="date">
+          <option value="">data</option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </div>
+    </div> -->
   <div class="hero-body">
-    <!-- <div class="container"> -->
       <div v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="20">
         <div class="image-grid">
-
           <div class="image-item" v-for="photo in photos">
-            <a :href="'photos/' + photo.id" target="_blank" />
-              <img :src="'/arquigrafia-images/' + photo.id + '_home.jpg'" alt="">
+            <a :href="'photos/' + photo.id">
+              <img :src="'/arquigrafia-images/' + photo.id + '_view.jpg'" alt="">
+              <div class="overlay">
+                <p class="image-title">{{photo.name}}</p>
+              </div>
             </a>
-            <div class="overlay">
-            </div>
           </div>
         </div>
       </div>
       <img v-if="loading === true" id="loader-img" src="" />
-    <!-- </div> -->
   </div>
   </div>
 </template>
@@ -49,7 +86,8 @@ import infiniteScroll from 'vue-infinite-scroll'
             this.loading = false;
           });
         }
-      }
+      },
+
     }
   };
 
